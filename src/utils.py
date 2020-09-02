c77d8d0c89687702d87d2aad89547763d0786ef1
JhoLee <jho.lee@kakao.com>
2020-09-02 21:59:38 +0900

    ADD: Add timestamp into log

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import datetime
 import inspect
 
 
@@ -5,9 +6,10 @@ def print_log(msg: str, level: str = "debug"):
     stack = inspect.stack()
     class_name = stack[1][0].f_locals["self"].__class__.__name__
     method_name = stack[1][0].f_code.co_name
-    print("[{level}] {cls_name}.{func_name}(): {msg}".format(
-        level=level,
+    print("[{level}] {timestamp}: {cls_name}.{func_name}(): {msg}".format(
+        level=level.upper(),
+        timestamp=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         cls_name=class_name,
         func_name=method_name,
-        msg=msg
+        msg=msg,
     ))
